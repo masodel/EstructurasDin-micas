0f21b65d2fe1e49c87e3ae8d0def4aa82f2272ab
Mauro Delgado <maurods@uamv.edu.ni>
2025-10-17 10:36:21 -0600

    10-17

--- Estrcturada1/Form1.cs
+++ Estrcturada1/Form1.cs
@@ -10,11 +10,96 @@ using System.Windows.Forms;
 
 namespace Estrcturada1
 {
-    public partial class Form1 : Form
+    public partial class FrmPilaM : Form
     {
-        public Form1()
+        int cont = 0;
+        int num1 = 0;
+        int num2 = 0;
+
+        Stack<int> pila = new Stack<int>();
+        public FrmPilaM()
         {
             InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int edad = int.Parse(tbAgregar.Text);
+            pila.Push(edad);
+            cont++;
+            Mostrar();
+        }
+
+        private void Mostrar()
+        {
+            string msn = "";
+
+            foreach (int edad in pila)
+            {
+                msn += " " + edad + " |";
+            }
+
+            lblEdades.Text = msn;
+
+            tbAgregar.Clear();
+            tbAgregar.Focus();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+            {
+                pila.Pop();
+                Mostrar();
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía.");
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+                MessageBox.Show("Elemento en la cima: " + pila.Peek());
+
+            else
+                MessageBox.Show("La pila está vacía.");
+        }
+
+        private void ordenar()
+        {
+            Stack<int> pilaAux = new Stack<int>();
+
+            while (pila.Count > 0)
+            {
+                int temp = pila.Pop();
+
+                while (pilaAux.Count > 0 && pilaAux.Peek() > temp)
+                {
+                    pila.Push(pilaAux.Pop());
+                }
+
+                pilaAux.Push(temp);
+            }
+            pila = pilaAux;
+
+            string msn = "";
+
+            foreach (int edad in pila)
+            {
+                msn += " " + edad + " |";
+            }
+
+            lblEdades2.Text = msn;
+
+            tbAgregar.Clear();
+            tbAgregar.Focus();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            ordenar();
         }
     }
 }
